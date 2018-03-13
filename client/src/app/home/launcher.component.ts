818f9d46e104240bc01c5c82aa18bc6bc1d04b06
Nate Nelles <nnelles1@gmail.com>
2018-03-13 13:46:39 -0500

    form validation

--- client/src/app/home/launcher.component.ts
+++ client/src/app/home/launcher.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromevent';
 import { Router } from '@angular/router';
@@ -11,22 +12,12 @@ import { login } from 'app/core/store/actions/session.actions';
 
 @Component({
     selector: 'app-launcher',
-    template: `
-        <div class="container">
-            <div class="form-group">
-                <input #username placeholder="Username" class="form-control" />
-                <input #password placeholder="Password" class="form-control" />
-                <div class ="group-control">
-                    <button ng-disabled="enableButton$" #btnLaunch class="btn btn-success btn-lg" (click)="clicked()">Go</button>
-                </div>
-            </div>
-        </div>
-    `
+    templateUrl: './launcher.component.html'
 })
 export class LauncherComponent implements AfterViewInit {
+    model: any = {};
     @ViewChild('username') username: ElementRef;
     @ViewChild('password') password: ElementRef;
-    @ViewChild('btnLaunch') launch: ElementRef;
 
     userInfo$: Observable<UserInfo>;
     enableButton$: Observable<boolean>;
@@ -35,19 +26,19 @@ export class LauncherComponent implements AfterViewInit {
         private store: Store<fromRoot.State>) {
         this.userInfo$ = store.select(fromRoot.getUserInfo);
         this.enableButton$ = store.select(fromRoot.getPendingSessionUpdate).map(b => !b);
-        // this.userInfo$.subscribe(userInfo => {
-        //     if (!!userInfo) {
-        //       this.router.navigate(['home']);
-        //     }
-        // });
+        this.userInfo$.subscribe(userInfo => {
+             if (!!userInfo) {
+               this.router.navigate(['home']);
+             }
+        });
+
     }
 
     ngAfterViewInit() {
 
     }
 
-    clicked() {
-        // TODO validate
-        this.store.dispatch(login(this.username.nativeElement.value, this.password.nativeElement.value));
+    login(myForm: NgForm) {
+        this.store.dispatch(login(this.model.username, this.model.password));
     }
 }
